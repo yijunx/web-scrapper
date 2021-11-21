1ed76c487a97bc0ecdfda9f1f70107898222090c
yijunxu <yijunx@umich.edu>
2021-11-21 14:33:51 +0800

    need think about how not be considered a robot

--- app/fetch.sh
+++ app/fetch.sh
@@ -5,7 +5,7 @@ curl $SCRAPPING_URL_SG \
 echo "pulling $SCRAPPING_URL_HK"
 curl $SCRAPPING_URL_HK \
      -H 'user-agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.66 Safari/537.36' \
-     -H 'accept-language: en-US,en;q=0.9' --compressed> to_parse_hk.html
+     -H 'accept-language: en-US,en;q=0.9' --compressed > to_parse_hk.html
 echo "pulling $SCRAPPING_URL_FR"
 curl $SCRAPPING_URL_FR \
      -H 'user-agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.66 Safari/537.36' \
@@ -13,6 +13,6 @@ curl $SCRAPPING_URL_FR \
 echo "pulling $SCRAPPING_URL_UK"
 curl $SCRAPPING_URL_UK \
      -H 'user-agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.66 Safari/537.36' \
-     -H 'accept-language: en-US,en;q=0.9' --compressed  > to_parse_uk.html
+     -H 'accept-language: en-US,en;q=0.9' --compressed > to_parse_uk.html
 
 # 'https://www.hermes.com/sg/en/category/women/bags-and-small-leather-goods/bags-and-clutches/'
